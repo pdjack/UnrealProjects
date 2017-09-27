1bcf2f748dd12517478b8db931f4578f50433e03
Jack <doongji76@gmail.com>
2017-09-27 17:52:33 +0900

    NewMap

--- Study_Cpp_01/Source/Study_Cpp_01/PositionReport.cpp
+++ Study_Cpp_01/Source/Study_Cpp_01/PositionReport.cpp
@@ -1,6 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "PositionReport.h"
+
+#include "Study_Cpp_01.h"
+//#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -10,7 +15,8 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
+
 }
 
 
@@ -20,7 +26,11 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString(); // "X=something, Y=somthing else, etc";
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos );
+
 }
 
 
